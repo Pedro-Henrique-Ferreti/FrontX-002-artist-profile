362a3af954cd1d13516f093d763fc62ad1e28d5c
Pedro Henrique <phfs28@gmail.com>
2021-11-02 09:49:32 -0300

    Created the BaseContainer component

---
+++ src/components/common/BaseContainer.vue
@@ -0,0 +1,45 @@
+<template>
+  <component :is="tag" class="base-container" :class="containerClasses">
+    <slot />
+  </component>
+</template>
+
+<script>
+export default {
+  name: 'BaseContainer',
+  props: {
+    tag: {
+      type: String,
+      default: 'div',
+    },
+    padding: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  computed: {
+    containerClasses() {
+      return {
+        'base-container--no-padding': !this.padding,
+      };
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.base-container {
+  flex-direction: column;
+  width: 100%;
+  max-width: 1824px;
+  padding-left: 32px;
+  padding-right: 32px;
+  margin-left: auto;
+  margin-right: auto;
+  &--no-padding {
+    max-width: 1760px;
+    padding-left: 0;
+    padding-right: 0;
+  }
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -2,7 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import store from './store/index.js';
 
+import globalComponents from '@/plugins/globalComponents.js';
+
 const appInstance = createApp(App);
 
 appInstance.use(store);
+appInstance.use(globalComponents);
 appInstance.mount('#app');
