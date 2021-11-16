2ab405eb54ddfddbeeff0d3ab1dd96f9fd6f92e6
Pedro Henrique <phfs28@gmail.com>
2021-11-16 11:25:03 -0300

    Created HomeCarousel and its components

--- src/components/HomeCarousel.vue
+++ src/components/HomeCarousel.vue
@@ -1,28 +1,45 @@
 <template>
   <div class="carousel">
     <BaseContainer class="carousel__container">
-      <HomeCarouselIndicators :slides="4" :active="active" />
+      <Splide class="carousel__splide" :options="carouselOptions">
+        <SplideSlide class="carousel__slide">
+          <DescriptionSlide />
+        </SplideSlide>
+        <SplideSlide v-for="n in 2" :key="n">
+          <img :src="`https://picsum.photos/id/${n * 2}/50/200`" alt="Sample 1" />
+        </SplideSlide>
+      </Splide>
     </BaseContainer>
   </div>
 </template>
 
 <script>
-import HomeCarouselIndicators from '@/components/HomeCarouselIndicators.vue';
+import DescriptionSlide from '@/components/HomeCarouselDescriptionSlide.vue';
+import { Splide, SplideSlide } from '@splidejs/vue-splide';
+import '@splidejs/splide/dist/css/splide.min.css';
 
 export default {
   name: 'HomeCarousel',
   components: {
-    HomeCarouselIndicators,
+    DescriptionSlide,
+    Splide,
+    SplideSlide,
   },
   data() {
     return {
-      active: 1,
+      carouselOptions: {
+        // autoplay: true,
+        // interval: 8000,
+        rewind: true,
+        direction: 'ttb',
+        height: '175px',
+        classes: {
+          arrows: 'carousel__arrows',
+          pagination: 'carousel__pagination',
+          page: 'carousel__pagination__item',
+        },
+      },
     };
-  },
-  mounted() {
-    setTimeout(() => {
-      this.active = 2;
-    }, 3000);
   },
 };
 </script>
@@ -37,8 +54,48 @@ export default {
     }
   }
   &__container {
-    padding-right: 0;
-    flex-direction: row !important;
+    padding-right: 0 !important;
+    @media (max-width: $desktop-up) {
+      align-items: center;
+    }
+  }
+  &__splide {
+    width: fit-content;
+  }
+  &__slide {
+    width: 100%;
+  }
+  ::v-deep() {
+    .splide__track {
+      padding-left: 32px;
+      @media (min-width: $desktop-up) {
+        padding-left: 70px;
+      }
+    }
+    .carousel__arrows {
+      display: none;
+    }
+    .carousel__pagination {
+      flex-direction: column;
+      position: absolute;
+      li:not(:last-child) {
+        margin-bottom: 24px;
+        height: 12px;
+      }
+      &__item {
+        background-color: $gray-3;
+        width: 12px;
+        height: 12px;
+        border-radius: 50%;
+        cursor: pointer;
+        transition: all .2s ease-in-out;
+        &.is-active {
+          background-color: $primary;
+          height: 24px;
+          border-radius: 8px;
+        }
+      }
+    }
   }
 }
 </style>
---
+++ src/components/HomeCarouselDescriptionSlide.vue
@@ -0,0 +1,26 @@
+<template>
+  <BaseContainer class="description-slide">
+    <p class="description-slide__description">
+      Cantora, atriz, compositora e apresentadora estadunidense. Em oito anos de carreira solo, venci dois Grammy Awards (Sweetener e Rain On Me). Não sou ariana, e nem grande.
+    </p>
+  </BaseContainer>
+</template>
+
+<script>
+export default {
+  name: 'HomeCarouselDescriptionSlide',
+};
+</script>
+
+<style lang="scss" scoped>
+.description-slide {
+  padding-left: 0 !important;
+  max-width: 600px !important;
+  &__description {
+    line-height: normal;
+    @media (min-width: $desktop-up) {
+      font-size: 28px;
+    }
+  }
+}
+</style>
