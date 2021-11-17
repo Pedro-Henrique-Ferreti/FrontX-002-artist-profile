6dd05ec240a8ed256bf1d42d82b5beb1f60ca94e
Pedro Henrique <phfs28@gmail.com>
2021-11-17 11:04:16 -0300

    Added songs and tv shows data

--- src/data/index.js
+++ src/data/index.js
@@ -24,3 +24,94 @@ export const songList = [
     url: '/audio/Pizza-Parlor-Theme-2012.mp3',
   },
 ];
+
+export const FEATURED_SONGS = [
+  {
+    id: 1,
+    name: 'Positions',
+    releaseYear: '2020',
+    hasExplicitContent: true,
+    image: 'Positions.png',
+  },
+  {
+    id: 2,
+    name: '7 Rings',
+    releaseYear: '2018',
+    hasExplicitContent: true,
+    image: 'Thank-u-Next.png',
+  },
+  {
+    id: 3,
+    name: 'thank u, next',
+    releaseYear: '2019',
+    hasExplicitContent: true,
+    image: 'Thank-u-Next.png',
+  },
+  {
+    id: 4,
+    name: 'Dangerous Woman',
+    releaseYear: '2019',
+    hasExplicitContent: true,
+    image: 'Dangerous-Woman.png',
+  },
+];
+
+export const TV_SHOWS = [
+  {
+    id: 1,
+    name: 'The Voice',
+    releaseYear: '2021',
+    image: 'Carpool-Karaoke.png',
+  },
+  {
+    id: 2,
+    name: 'Carpool Karaoke',
+    releaseYear: '2018',
+    image: 'The-Voice.png',
+  },
+];
+
+export const ALBUMS = [
+  {
+    id: 1,
+    name: 'Positions',
+    releaseYear: '2020',
+    hasExplicitContent: true,
+    image: 'Positions.png',
+  },
+  {
+    id: 2,
+    name: 'thank u, next',
+    releaseYear: '2019',
+    hasExplicitContent: true,
+    image: 'Thank-u-Next.png',
+  },
+  {
+    id: 3,
+    name: 'Sweetener',
+    releaseYear: '2018',
+    hasExplicitContent: true,
+    image: 'Sweetener.png',
+  },
+  {
+    id: 4,
+    name: 'Dangerous Woman',
+    releaseYear: '2019',
+    hasExplicitContent: true,
+    image: 'Dangerous-Woman.png',
+  },
+  {
+    id: 5,
+    name: 'My Everything',
+    releaseYear: '2014',
+    hasExplicitContent: true,
+    image: 'My-Everything.png',
+  },
+  {
+    id: 6,
+    name: 'Yours Truly',
+    releaseYear: '2013',
+    hasExplicitContent: true,
+    image: 'Yours-Trully.png',
+  },
+];
